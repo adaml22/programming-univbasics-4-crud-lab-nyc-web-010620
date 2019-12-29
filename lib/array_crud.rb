d2e8a56c117e98e9b48095600783677570bf494a
Adam Longman <boardhead89@gmail.com>
2019-12-29 21:34:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,25 +17,34 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop(1)
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.shift(1)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 2
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 4
+  array[index_number] = "totally"
+  return array[index_number]
 end
